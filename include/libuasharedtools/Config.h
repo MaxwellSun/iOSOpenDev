2817b4eb62e0b5077757531791ce9ccca0efbf84
Enea Gjoka <enea@unlimapps.com>
2017-02-23 21:32:54 -0500

    UASHAREDTOOLS - 2.1r-48

--- include/libuasharedtools/Config.h
+++ include/libuasharedtools/Config.h
@@ -1,4 +1,4 @@
 // Do not edit
-#define kUAGAIAppVersion @"2.1r-47"
+#define kUAGAIAppVersion @"2.1r-48"
 #define kUAGAIAppName @"uasharedtools"
-// Updated on Thu Feb 23 13:51:03 EST 2017
+// Updated on Thu Feb 23 21:31:47 EST 2017
