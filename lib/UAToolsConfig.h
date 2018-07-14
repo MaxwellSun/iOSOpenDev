c762e2d513157951838247f8c9a3eda83284ebde
Enea <enea@unlimapps.com>
2018-07-14 04:02:42 -0400

    UASHAREDTOOLS - 2.2r-42

--- lib/UAToolsConfig.h
+++ lib/UAToolsConfig.h
@@ -1,4 +1,4 @@
 // Do not edit
-#define kUAGAIAppVersion @"2.2r-41"
+#define kUAGAIAppVersion @"2.2r-42"
 #define kUAGAIAppName @"uasharedtools"
-// Updated on Sat Jul 14 03:50:27 EDT 2018
+// Updated on Sat Jul 14 04:00:55 EDT 2018
