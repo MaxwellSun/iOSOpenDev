4e5848e1913c2dd0fb03a854cb29d7b4b6e9ad32
Enea <enea09@gmail.com>
2018-11-08 15:43:04 -0800

    UASHAREDTOOLS - 2.2r-68

--- lib/UAToolsConfig.h
+++ lib/UAToolsConfig.h
@@ -1,4 +1,4 @@
 // Do not edit
-#define kUAGAIAppVersion @"2.2r-64"
+#define kUAGAIAppVersion @"2.2r-68"
 #define kUAGAIAppName @"uasharedtools"
-// Updated on Thu Nov 8 15:26:50 PST 2018
+// Updated on Thu Nov 8 15:39:45 PST 2018
